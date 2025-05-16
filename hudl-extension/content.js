e94b11de8d654a82fb585e9411c6e02132a1eb6f
cmont13 <cmontie0922@gmail.com>
2025-05-16 15:51:11 -0400

    Added Sample Videos

---
+++ hudl-extension/content.js
@@ -0,0 +1,4 @@
+let new_vid = "https://vj.hudl.com/366014/84387/83993206/c9GDW0W1QWZ266Rq_6500.mp4?v=55976339F57CDD08";
+
+var videoTags = document.getElementsByTagName("video")
+console.log(videoTags)
