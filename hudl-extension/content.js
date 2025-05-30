f9163a92e51d0c2cb671496d32455a22ab3f660e
cmont13 <cmontie0922@gmail.com>
2025-05-30 10:29:42 -0400

    Created Referee Detection Model

--- hudl-extension/content.js
+++ hudl-extension/content.js
@@ -1,4 +1,4 @@
 let new_vid = "https://vj.hudl.com/366014/84387/83993206/c9GDW0W1QWZ266Rq_6500.mp4?v=55976339F57CDD08";
 
-var videoTags = document.getElementsByTagName("video")
+var videoTags = document.querySelector("#vjs_video_1_THEOplayerDi > video:nth-child(3)")
 console.log(videoTags)
